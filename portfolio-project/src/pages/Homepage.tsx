5f6bfd959ee500035c59e0749ae3e775f3a9d55b
Frederik Farstad <Frederf@stud.ntnu.no>
2023-02-19 17:34:58 +0100

    attempted fix for pictures not displaying on deployment

--- portfolio-project/src/pages/Homepage.tsx
+++ portfolio-project/src/pages/Homepage.tsx
@@ -1,8 +1,10 @@
 import Introduction from "../components/Introduction";
 import Textbox1 from "../components/Textbox1";
 import { WebsiteCarbonBadge } from 'react-websitecarbon-badge';
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 import HoverText from "../components/HoverText";
+import portrait from "../../public/img/portrait.jpeg";
+import arrow from "../../public/img/arrow.png";
 
 function Homepage() {
 
@@ -11,15 +13,21 @@ function Homepage() {
     return (
       <>
         <div className="scrollbar-hide h-screen overflow-scroll relative w-screen gradient-background">
+
+            {/*Landing page*/}
             <Introduction pageStartRef={pageStartRef}/>
+
+            {/*Portfolio start*/}
             <div ref={pageStartRef} id="pageStart" className="pt-[10vh] sm:pt-[0vh]">
+
+              {/*First textbox and image*/}
               <div className="ml-[10vw] mr-[10vh] mb-[10vh] sm:ml-0 sm:mr-0 flex justify-evenly flex-wrap-reverse relative">
                 <div className="w-fit inline-block"> 
                   <Textbox1 />
                 </div>
                 <div className="w-[500px] h-[400px] inline-block relative mb-[5vh]">
-                  <img src="../../public/portrait.jpeg" className="absolute h-[300px] w-auto rounded-full right-0 sm:m-auto sm:relative top-[5vh] shadow-lg shadow-purple-800 select-none hover:scale-105 transition-all duration-200"/>
-                  <img src="../../public/arrow.png" className="absolute h-[100px] w-auto left-[10px] top-[75px] sm:hidden select-none"/>
+                  <img src={portrait} className="absolute h-[300px] w-auto rounded-full right-0 sm:m-auto sm:relative top-[5vh] shadow-lg shadow-purple-800 select-none hover:scale-105 transition-all duration-200"/>
+                  <img src={arrow} className="absolute h-[100px] w-auto left-[10px] top-[75px] sm:hidden select-none"/>
                   <div className="absolute w-auto left-0 rotate-12 top-[150px] font-ModerneSans text-white text-2xl sm:hidden">
                     <HoverText text="That's" />
                     <p className="inline text-base select-none"> </p>
@@ -27,6 +35,7 @@ function Homepage() {
                   </div>
                 </div>
               </div>
+
               <div className="ml-[55vw] mb-[10vh]">
                   
               </div>
@@ -34,6 +43,8 @@ function Homepage() {
                   
               </div>
             </div>
+
+            {/*WebsiteCarbonbadge*/}
             <div className="absolute m-0 w-fit right-[1vw]">
               <WebsiteCarbonBadge dark={true} url="farstad.dev"/>
             </div>
