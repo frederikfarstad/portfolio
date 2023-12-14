828d8acdd112ebbba6d479cb6764fc0a49b97cf3
Frederik Farstad <Frederf@stud.ntnu.no>
2023-12-14 20:42:39 +0100

    feat: implement functional navbar
    ref: #2

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+import styles from "./Navbar.module.css";
+import { elastic as Menu } from "react-burger-menu";
+import MenuStyles from "./MenuStyles.ts";
+import githubLogo from "../../assets/github.svg";
+import linkedinLogo from "../../assets/linkedin.svg";
+
+const Navbar = () => {
+  return (
+    <section className={styles.navbarWrapper}>
+      <Menu styles={MenuStyles}>
+        <a href="/">Home</a>
+        <a href="/resume">Resume</a>
+        <a href="/projects">Projects</a>
+      </Menu>
+      <div className={styles.socialsWrapper}>
+        <a
+          className={styles.logo}
+          href="https://github.com/frederikfarstad"
+          target="_blank">
+          <img src={githubLogo} alt="Github logo" />
+        </a>
+        <a
+          className={styles.logo}
+          href="https://www.linkedin.com/in/frederik-andreas-brunvoll-farstad/"
+          target="_blank">
+          <img src={linkedinLogo} alt="Linkedin logo" />
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
