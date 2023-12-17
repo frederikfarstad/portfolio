f7fa7b6a63b6de2ff4c682768f1e6e75d4d2f73d
Frederik Farstad <92976330+frederikfarstad@users.noreply.github.com>
2023-12-17 21:52:17 +0100

    8 create basic about page (#18)
    * feat: implement basic about page
    
    ref: #8
    
    * fix: fix build error
    
    * fix: fix vercel deployment

---
+++ src/components/mail/Mail.tsx
@@ -0,0 +1,7 @@
+import styles from "./Mail.module.css";
+
+const Mail = () => {
+  return <section className={styles.mailSection}></section>;
+};
+
+export default Mail;
---
+++ src/components/seperators/ClockwiseSeperator.tsx
@@ -0,0 +1,7 @@
+import styles from "./Seperator.module.css";
+
+const ClockwiseSeperator = () => {
+  return <div className={styles.clock}></div>;
+};
+
+export default ClockwiseSeperator;
